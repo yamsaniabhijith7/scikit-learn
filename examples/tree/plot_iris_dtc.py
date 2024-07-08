feca196b065d5173f857dc7ed4cf58af8eebf2c8
Abhijith Yamsani <yamsani.abhijith7@gmail.com>
2024-07-08 17:52:37 -0400

    Fixed UserWarning in plot_iris_dtc example

--- examples/tree/plot_iris_dtc.py
+++ examples/tree/plot_iris_dtc.py
@@ -18,15 +18,15 @@ We also show the tree structure of a model built on all of the features.
 # %%
 # First load the copy of the Iris dataset shipped with scikit-learn:
 from sklearn.datasets import load_iris
+from sklearn.tree import plot_tree
+
 
 iris = load_iris()
-
 
 # %%
 # Display the decision functions of trees trained on all pairs of features.
 import matplotlib.pyplot as plt
 import numpy as np
-
 from sklearn.datasets import load_iris
 from sklearn.inspection import DecisionBoundaryDisplay
 from sklearn.tree import DecisionTreeClassifier
@@ -35,7 +35,6 @@ from sklearn.tree import DecisionTreeClassifier
 n_classes = 3
 plot_colors = "ryb"
 plot_step = 0.02
-
 
 for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]]):
     # We only take the two corresponding features
@@ -47,7 +46,7 @@ for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]])
 
     # Plot the decision boundary
     ax = plt.subplot(2, 3, pairidx + 1)
-    plt.tight_layout(h_pad=0.5, w_pad=0.5, pad=2.5)
+    plt.tight_layout(pad=2.5)
     DecisionBoundaryDisplay.from_estimator(
         clf,
         X,
@@ -66,19 +65,16 @@ for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]])
             X[idx, 1],
             c=color,
             label=iris.target_names[i],
-            edgecolor="black",
+            edgecolors="black",  # Changed parameter name to 'edgecolors'
             s=15,
         )
 
 plt.suptitle("Decision surface of decision trees trained on pairs of features")
 plt.legend(loc="lower right", borderpad=0, handletextpad=0)
-_ = plt.axis("tight")
+plt.axis("tight")
 
 # %%
 # Display the structure of a single decision tree trained on all the features
-# together.
-from sklearn.tree import plot_tree
-
 plt.figure()
 clf = DecisionTreeClassifier().fit(iris.data, iris.target)
 plot_tree(clf, filled=True)
